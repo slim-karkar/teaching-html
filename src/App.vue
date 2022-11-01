2affe54ea83de3676edf21283d4fca600aaa9e0f
slim-karkar <104144900+slim-karkar@users.noreply.github.com>
2022-11-01 22:25:55 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
 <template>
-  <div>Hello {{ value }}</div>
+    <div>Hello {{ value }}
+   <ul>
+  <li>👋 Hi, I’m @slim-karkar</li>
+  <li>🌱 I’m currently teaching Bioinformatics @ u-bordeaux.fr</li>
+  <li>💞️ I’m looking to collaborate on educative and pedagogical ressources for STEM and coding</li>
+  <li>📫 How to reach me : DM or email@: first.last [at] institution</li>
+</ul>
+
+
+
+ </div>
 </template>
 
 <script>
@@ -7,7 +17,7 @@ export default {
   name: "App",
   data() {
     return {
-      value: "World"
+      value: ""
     };
   }
 };
